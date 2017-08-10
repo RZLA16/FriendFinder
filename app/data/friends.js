67e6b08cb45c70547ca970d970860d4683089e69
rudyz <rstar026@yahoo.com>
2017-08-09 21:50:53 -0700

    first push to heroku

---
+++ app/data/friends.js
@@ -0,0 +1,57 @@
+// DATA
+// Below data will hold all of the reserved tables.
+
+var friendsArray = [
+  
+  {
+  	friendName:"Jim Halpert",
+  	friendPhoto:"https://images-na.ssl-images-amazon.com/images/M/MV5BMTc3MzY3MjQ3OV5BMl5BanBnXkFtZTcwODI3NjQxMw@@._V1_UY317_CR6,0,214,317_AL_.jpg",
+  	friendScores:["5","1","4","4","5","1","2","5","4","1"]
+  },
+
+  {
+  	friendName:"Michael Scott",
+  	friendPhoto:"https://images-na.ssl-images-amazon.com/images/M/MV5BMjAzMjkzNzA0M15BMl5BanBnXkFtZTcwNzMyNjU3MQ@@._V1_UY317_CR6,0,214,317_AL_.jpg",
+  	friendScores:["4","2","5","1","3","2","2","1","3","2"]
+  },
+
+  {
+  	friendName:"Pam Beesly",
+  	friendPhoto:"https://images-na.ssl-images-amazon.com/images/M/MV5BMTMyOTYzODQ5MF5BMl5BanBnXkFtZTcwMjE3MDgzMQ@@._V1_UY317_CR20,0,214,317_AL_.jpg",
+  	friendScores:["5","2","2","2","4","1","3","2","5","5"]
+  },
+
+  {
+  	friendName:"Dwight Schrute",
+  	friendPhoto:"https://images-na.ssl-images-amazon.com/images/M/MV5BMTQwNDM1MjQ0M15BMl5BanBnXkFtZTcwNTkzNDczMg@@._V1_UY317_CR13,0,214,317_AL_.jpg",
+  	friendScores:["3","3","4","2","2","1","3","2","2","3"]
+  },
+
+  {
+  	friendName:"Angela Martin",
+  	friendPhoto:"https://images-na.ssl-images-amazon.com/images/M/MV5BMGMxOGYwYzEtMzgxYS00MmZmLWFhZmYtNDFjOWFjYjgyOWRiXkEyXkFqcGdeQXVyMjQwMDg0Ng@@._V1_UX214_CR0,0,214,317_AL_.jpg",
+  	friendScores:["4","3","4","1","5","2","5","3","1","4"]
+  },
+
+  {
+  	friendName:"Kelly Kapoor",
+  	friendPhoto:"https://images-na.ssl-images-amazon.com/images/M/MV5BNzQ2OTUzNDU1MF5BMl5BanBnXkFtZTcwODQ2MTYyNw@@._V1_UX214_CR0,0,214,317_AL_.jpg",
+  	friendScores:["4","4","2","3","2","2","3","2","4","5"]
+  },
+
+  {
+  	friendName:"Ryan Howard",
+  	friendPhoto:"https://images-na.ssl-images-amazon.com/images/M/MV5BMTU0MTg4MzEyMl5BMl5BanBnXkFtZTcwMzUxMzAyOA@@._V1_UX214_CR0,0,214,317_AL_.jpg",
+  	friendScores:["2","2","4","2","1","4","3","5","2","3"]
+  },
+
+  {
+  	friendName:"Kevin Malone",
+  	friendPhoto:"https://pbs.twimg.com/profile_images/118888142/Brian_Baumgartner_134198_400x400.jpg",
+  	friendScores:["3","2","4","2","1","1","3","5","2","1"]
+  }
+  
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
